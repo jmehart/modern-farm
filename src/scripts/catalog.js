65a2b043e3b74bce10db34c535cbfdf579688586
Jaimie Hart <hartjme@gmail.com>
2022-01-27 17:01:18 -0500

    Push it real good

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,6 +1,7 @@
 export const catalog = (harvestedFoodArray) => {
     let templateString = ""
     for (const food of harvestedFoodArray) {
+
         templateString += `<section class ="plant">${food.type}</section>`
     }
     return templateString
